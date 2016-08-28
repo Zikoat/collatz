ca55290a1709aed8559fd9502b1ada41ffa7acdb
sigurd-laptop <sschoeler99@gmail.com>
2016-08-28 17:27:01 +0200

    last finish-upper

--- collatzMedium.js
+++ collatzMedium.js
@@ -5,20 +5,28 @@ function collatz(n) { // returns the next number in the sequence
 function collatzSteps(n) { // returns how many steps you need to get to 1
 	let count = 0;
 	while (n != 1) {
+		if (cache.hasOwnProperty(n)){
+			console.log(n +' : '+ cache[n] +' + '+ count +' = '+ (cache[n] + count));
+			return cache[n] + count;
+			// add this cache's stuff to stepamount, return that and exit function
+		}
 		n = collatz(n);
 		count++;
+		cache[n] = count;
+		console.log(cache);
 	}
 	return count;
 }
 
-// maxN defaults to 10'000
-var maxN = typeof process.argv[2] !== 'undefined' ? Number(process.argv[2]) : 10000;
+// maxN defaults to 10'000, also called limit
+var maxN = typeof process.argv[2] !== 'undefined' ? Number(process.argv[2]) : 1000000;
 
-var highestN = 0;
-var highest = 0;
-
+var highestN = 0; // maxN
+var highest = 0; // maxOrbit
+var cache = {};
 var timeStart = process.hrtime(); // start timing
-var n = 0;
+var n = 3;
+var totalCount = 0;
 
 while (n < maxN) {
 	n++;
@@ -26,7 +34,7 @@ while (n < maxN) {
 	if (stepAmount > highest) {
 		highestN = n;
 		highest = stepAmount;
-		if(maxN > 999999) console.log(highestN +" : "+ highest);
+		// if(maxN > 999999) console.log(highestN +" : "+ highest);
 	}
 }
 
@@ -38,3 +46,4 @@ function timeToMs(time) { // converts process.hrtime to ms
 
 console.log('operation for ' + n + ' sequences took ' + timeToMs(timeDifference) + ' ms.');
 console.log('longest sequence: ' + highestN + ' : ' + collatzSteps(highestN));
+// console.log('total count: ' + totalCount);
